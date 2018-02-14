7f05f38baf7c84447bf329baa90b43c8c865a439
Jesús Neri <35941471+jesusneri@users.noreply.github.com>
2018-02-14 11:54:39 -0600

    Add files via upload

---
+++ js/mario.js
@@ -0,0 +1,208 @@
+
+$(document).ready(function(){
+    var i = 0;
+    var num = 0;
+    while(i < 500){
+    setTimeout(function(){
+               running();
+               },num + 800);
+         num = num + 800;
+
+    setTimeout(function(){
+               normal();
+               }, num + 800);
+         num = num + 800;
+
+    i++;
+    }
+});
+
+
+
+function normal(){
+
+     $("#one_thirtteen").css('background', 'none');
+     $("#one_fourteen").css('background', 'none');
+     $("#one_fiveteen").css('background', 'none');
+     $("#two_fourteen").css('background', 'none');
+     $("#two_fiveteen").css('background', 'none');
+     $("#three_thirtteen").css('background', 'none');
+     $("#three_fourteen").css('background', 'none');
+     $("#three_fiveteen").css('background', 'none');
+     $("#four_fourteen").css('background', 'none');
+     $("#four_fiveteen").css('background', 'none');
+     $("#five_fiveteen").css('background', 'none');
+     $("#six_fourteen").css('background', 'none');
+     $("#seven_thirtteen").css('background', 'none');
+     $("#seven_fourteen").css('background', 'none');
+     $("#eight_twelve").css('background', 'none');
+     $("#eight_thirtteen").css('background', 'none');
+     $("#eight_eleven").css('background', 'none');
+     $("#nine_twelve").css('background', '#e74c3c');
+     $("#ten_twelve").css('background', '#e74c3c');
+     $("#nine_ten").css('background', '#2E86C1');
+     $("#nine_thirtteen").css('background', '#e74c3c');
+     $("#ten_thirtteen").css('background', '#e74c3c');
+     $("#ten_fourteen").css('background', '#e74c3c');
+     $("#eleven_twelve").css('background', '#e74c3c');
+     $("#eleven_thirtteen").css('background', '#FCF3CF');
+     $("#eleven_fourteen").css('background', '#FCF3CF');
+     $("#eleven_fiveteen").css('background', 'none');
+     $("#ten_fiveteen").css('background', 'none');
+     $("#twelve_fiveteen").css('background', 'none');
+     $("#thirtteen_fiveteen").css('background', 'none');
+     $("#twelve_fourteen").css('background', '#FCF3CF');
+     $("#thirtteen_fourteen").css('background', '#FCF3CF');
+     $("#twelve_thirtteen").css('background', '#FCF3CF');
+     $("#thirtteen_thirtteen").css('background', '#FCF3CF');
+     $("#twelve_twelve").css('background', '#FCF3CF');
+     $("#fourteen_twelve").css('background', '#2E86C1');
+     $("#fourteen_eleven").css('background', '#2E86C1');
+     $("#fourteen_ten").css('background', 'none');
+     $("#fiveteen_twelve").css('background', '#B9770E');
+     $("#fiveteen_eleven").css('background', '#B9770E');
+     $("#fiveteen_thirtteen").css('background', '#B9770E');
+     $("#sixteen_twelve").css('background', '#B9770E');
+     $("#sixteen_eleven").css('background', '#B9770E');
+     $("#sixteen_thirtteen").css('background', '#B9770E');
+     $("#sixteen_fourteen").css('background', '#B9770E');
+     $("#fourteen_ten").css('background', '#2E86C1');
+     $("#fourteen_nine").css('background', 'none');
+     $("#fourteen_eight").css('background', 'none');
+     $("#fiveteen_eight").css('background', 'none');
+     $("#fiveteen_seven").css('background', 'none');
+     $("#fiveteen_six").css('background', '#B9770E');
+     $("#fiveteen_five").css('background', '#B9770E');
+     $("#sixteen_six").css('background', '#B9770E');
+     $("#sixteen_five").css('background', '#B9770E');
+     $("#sixteen_four").css('background', '#B9770E');
+     $("#sixteen_three").css('background', '#B9770E');
+     $("#fiveteen_four").css('background', '#B9770E');
+     $("#fiveteen_three").css('background', 'none');
+     $("#fiveteen_two").css('background', 'none');
+     $("#fourteen_two").css('background', 'none');
+     $("#fourteen_three").css('background', 'none');
+     $("#fourteen_four").css('background', 'none');
+     $("#thirtteen_three").css('background', '#FCF3CF');
+     $("#thirtteen_four").css('background', '#FCF3CF');
+     $("#thirtteen_five").css('background', '#2E86C1');
+     $("#twelve_four").css('background', '#FCF3CF');
+     $("#twelve_three").css('background', '#FCF3CF');
+     $("#twelve_five").css('background', '#FCF3CF');
+     $("#eleven_three").css('background', '#FCF3CF');
+     $("#eleven_four").css('background', '#FCF3CF');
+     $("#eleven_five").css('background', '#e74c3c');
+     $("#ten_seven").css('background', '#2E86C1');
+     $("#eleven_seven").css('background', '#F4D03F');
+     $("#ten_eight").css('background', '#2E86C1');
+     $("#eight_eight").css('background', '#e74c3c');
+     $("#eight_seven").css('background', '#2E86C1');
+     $("#nine_seven").css('background', '#2E86C1');
+     $("#nine_nine").css('background', '#e74c3c');
+     $("#ten_eleven").css('background', '#e74c3c');
+     $("#nine_three").css('background', 'none');
+     $("#eight_three").css('background', 'none');
+     $("#eight_four").css('background', 'none');
+     $("#nine_two").css('background', 'none');
+     $("#ten_two").css('background', 'none');
+     $("#ten_one").css('background', 'none');
+     $("#eleven_two").css('background', 'none');
+     $("#eleven_one").css('background', 'none');
+     $("#twelve_two").css('background', 'none');
+
+}
+
+function running(){
+     $("#one_thirtteen").css('background', '#FCF3CF');
+     $("#one_fourteen").css('background', '#FCF3CF');
+     $("#one_fiveteen").css('background', '#FCF3CF');
+     $("#two_fourteen").css('background', '#FCF3CF');
+     $("#two_fiveteen").css('background', '#FCF3CF');
+     $("#three_thirtteen").css('background', '#e74c3c');
+     $("#three_fourteen").css('background', '#e74c3c');
+     $("#three_fiveteen").css('background', '#e74c3c');
+     $("#four_fourteen").css('background', '#e74c3c');
+     $("#four_fiveteen").css('background', '#e74c3c');
+     $("#five_fiveteen").css('background', '#e74c3c');
+     $("#six_fourteen").css('background', '#e74c3c');
+     $("#seven_thirtteen").css('background', '#e74c3c');
+     $("#seven_fourteen").css('background', '#e74c3c');
+     $("#eight_twelve").css('background', '#e74c3c');
+     $("#eight_thirtteen").css('background', '#e74c3c');
+     $("#eight_eleven").css('background', '#2E86C1');
+     $("#nine_twelve").css('background', '#2E86C1');
+     $("#ten_twelve").css('background', '#2E86C1');
+     $("#nine_ten").css('background', '#e74c3c');
+     $("#eight_thirtteen").css('background', 'none');
+     $("#nine_thirtteen").css('background', 'none');
+     $("#ten_thirtteen").css('background', 'none');
+     $("#ten_fourteen").css('background', 'none');
+     $("#eleven_twelve").css('background', '#F4D03F');
+     $("#eleven_thirtteen").css('background', '#2E86C1');
+     $("#eleven_fourteen").css('background', '#B9770E');
+     $("#eleven_fiveteen").css('background', '#B9770E');
+     $("#ten_fiveteen").css('background', '#B9770E');
+     $("#twelve_fiveteen").css('background', '#B9770E');
+     $("#thirtteen_fiveteen").css('background', '#B9770E');
+     $("#twelve_fourteen").css('background', '#B9770E');
+     $("#thirtteen_fourteen").css('background', '#B9770E');
+     $("#twelve_thirtteen").css('background', '#2E86C1');
+     $("#thirtteen_thirtteen").css('background', '#2E86C1');
+     $("#twelve_twelve").css('background', '#2E86C1');
+     $("#fourteen_twelve").css('background', 'none');
+     $("#fourteen_eleven").css('background', '#2E86C1');
+     $("#fourteen_ten").css('background', 'none');
+     $("#fiveteen_twelve").css('background', 'none');
+     $("#fiveteen_eleven").css('background', 'none');
+     $("#fiveteen_thirtteen").css('background', 'none');
+     $("#sixteen_twelve").css('background', 'none');
+     $("#sixteen_eleven").css('background', 'none');
+     $("#sixteen_thirtteen").css('background', 'none');
+     $("#sixteen_fourteen").css('background', 'none');
+     $("#fourteen_ten").css('background', '#2E86C1');
+     $("#fourteen_nine").css('background', '#2E86C1');
+     $("#fourteen_eight").css('background', '#2E86C1');
+     $("#fiveteen_eight").css('background', '#2E86C1');
+     $("#fiveteen_seven").css('background', '#2E86C1');
+     $("#fiveteen_six").css('background', '#2E86C1');
+     $("#fiveteen_five").css('background', '#2E86C1');
+     $("#sixteen_six").css('background', 'none');
+     $("#sixteen_five").css('background', 'none');
+     $("#sixteen_four").css('background', 'none');
+     $("#sixteen_three").css('background', 'none');
+     $("#fiveteen_four").css('background', 'none');
+     $("#fiveteen_three").css('background', 'none');
+     $("#fiveteen_two").css('background', '#B9770E');
+     $("#fourteen_two").css('background', '#B9770E');
+     $("#fourteen_three").css('background', '#B9770E');
+     $("#fourteen_four").css('background', '#B9770E');
+     $("#thirtteen_three").css('background', '#B9770E');
+     $("#thirtteen_four").css('background', '#B9770E');
+     $("#thirtteen_five").css('background', '#B9770E');
+     $("#twelve_four").css('background', '#B9770E');
+     $("#twelve_three").css('background', 'none');
+     $("#twelve_five").css('background', 'none');
+     $("#eleven_three").css('background', '#FCF3CF');
+     $("#eleven_four").css('background', 'none');
+     $("#eleven_five").css('background', 'none');
+     $("#ten_seven").css('background', ' #e74c3c');
+     $("#eleven_seven").css('background', '#e74c3c');
+     $("#ten_eight").css('background', '#e74c3c');
+     $("#eight_eight").css('background', '#2E86C1');
+     $("#eight_seven").css('background', '#e74c3c');
+     $("#nine_seven").css('background', '#e74c3c');
+     $("#nine_nine").css('background', '#2E86C1');
+     $("#ten_eleven").css('background', '#2E86C1');
+     $("#nine_three").css('background', '#e74c3c');
+     $("#eight_three").css('background', '#e74c3c');
+     $("#eight_four").css('background', '#e74c3c');
+     $("#nine_two").css('background', '#e74c3c');
+     $("#ten_two").css('background', '#FCF3CF');
+     $("#ten_one").css('background', '#FCF3CF');
+     $("#eleven_two").css('background', '#FCF3CF');
+     $("#eleven_one").css('background', '#FCF3CF');
+     $("#twelve_two").css('background', '#FCF3CF');
+
+}
+
+
